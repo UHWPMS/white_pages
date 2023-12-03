ae36c6d276f96b67609532f282859c9273b28b96
microtaryn <tarynet@hawaii.edu>
2023-12-03 09:41:18 -1000

    contacts in progress

--- app/Http/Controllers/DeptContactController.php
+++ app/Http/Controllers/DeptContactController.php
@@ -9,7 +9,11 @@ class DeptContactController extends Controller
 {
     public function index()
     {
-        $contacts = DB::select('select * from dept_contact_view');
-        return view('DeptContacts.dept_contacts',['data' => $contacts]);
+        $unique_persons = DB::select('select distinct(pid),code,name_of_record,' .
+           'per_email, per_phone, dept_name from person_role_view');
+        $combined_roles = DB::select('select pid, group_concat(role_name) as roles' . 
+           ' from person_role_view where role_name in ("Member", "Primary", "Secondary") group by pid');
+        return view('DeptContacts.dept_contacts',['unique_persons' => $unique_persons, 
+           'combined_roles' => $combined_roles]);
     }
 }
--- resources/views/DeptContacts/dept_contacts.blade.php
+++ resources/views/DeptContacts/dept_contacts.blade.php
@@ -1,8 +1,6 @@
-
 @extends('../Layout/layout')
 
 @section('content')
-
 <div class="container-fluid">
     <div>
         <h1>Manage Department Contacts</h1>
@@ -20,55 +18,78 @@
                         <th>Phone</th>
                         <th>Role</th>
                         <th>Edit Role</th>
-                        <th>Delete Role</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($data as $item)
-                        <tr id= {{"trow-". $item -> pid}}>
-                            <td>{{$item->name_of_record}}</td>
-                            <td>{{$item->code}}</td>
-                            <td>{{$item->dept_name}}</td>
-                            <td>{{$item->per_email}}</td>
-                            <td>{{$item->per_phone}}</td>
-                            <td>{{$item->role_name}}</td>
+                    @foreach($unique_persons as $index => $person)
+                        <tr id= {{"trow-". $person -> pid}}>
+                            <td>{{$person->name_of_record}}</td>
+                            <td>{{$person->code}}</td>
+                            <td>{{$person->dept_name}}</td>
+                            <td>{{$person->per_email}}</td>
+                            <td>{{$person->per_phone}}</td>
+                            <td>{{$combined_roles[$index]->roles}}</td>
                             <td style="text-align: center;">
-                                <button class="btn edit-button btn-custom"
+                            <button class="btn edit-button btn-custom"
                                     style="background-color: #86C2F1;"
-                                    data-pid="{{$item->pid}}"
-                                    data-name="{{$item->name_of_record}}"
-                                    data-role="{{$item->role_name}}"
-                                    data-toggle="modal" data-target="#editModal">
-                                
-                                    <i class="fa fa-pencil"></i>
-                                </button>
-                            </td>
-                            <td style="text-align: center;">
-                                    <button class="btn delete-button btn-custom"
-                                        style="background-color: #CB5D5D;"
-                                        data-pid="{{$item->pid}}"
-                                        data-name="{{$item->name_of_record}}"
-                                        data-role="{{$item->role_name}}">
-                                        <i class="fa fa-pencil"></i>
-                                    </button>
+                                    data-name="{{ $person->name_of_record }}"
+                                    data-id="{{$person->pid}}"
+                                    data-toggle="modal" data-target="#editPersonModal">
+                                <i class="fa fa-pencil"></i>
+                            </button>
                             </td>
            
                         </tr>
                     @endforeach
                 </tbody>
             </table>
-
         </div>
     </div>
 </div>
-@endsection
+<!-- editPersonModal -->
+<div class="modal fade" id="editPersonModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form action="{{ route('person_listings.update', ':personUsername' ) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="modal-header" style="background-color: #86C2F1;">
+                    <h5 class="modal-title" id="editModalLabel">Edit Person's Roles</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"
+                            id="edit-x-button">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="edit-username">Username</label>
+                        <input type="text" name="username" class="form-control" id="edit-username"
+                               required minlength="6" maxlength="60" title="Enter a username (6 to 60 characters)">
+                    </div>
+                    <div class="form-group">
+                        <label for="edit-person-name">Name of Record</label>
+                        <input type="text" name="name" class="form-control" id="edit-person-name"
+                               required minlength="2" maxlength="60" title="Enter a name (10 to 60 characters)">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="edit-close-button">
+                        Close
+                    </button>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <script>
-    $(document).ready(function () {
+   $(document).ready(function () {
         $("#dept-contacts-table").DataTable({
             "pagingType": "simple_numbers",
             "language": {
-                "emptyTable": "The Department Contacts table is empty",
-                "lengthMenu": "Display _MENU_ contacts",
+                "emptyTable": "The Person Listings table is empty",
+                "lengthMenu": "Display _MENU_ persons",
                 "loadingRecords": "Loading...",
                 "processing": "Processing...",
                 "zeroRecords": "No search results found",
@@ -76,12 +97,46 @@
                     "next": "Next",
                     "previous": "Previous"
                 }
-            },
-            label: false
-
+            }
         });
-        
+    });
     
+        // Function to handle the edit button click
+        $("#person-listings-table").on("click", ".edit-button", function () {
+            var personUsername = $(this).data("username");
+            var personName = $(this).data("name");
+            var personNameOfRecord = $(this).data("nameOfRecord");
+            var personJobTitle = $(this).data("jobTitle");
+            var personEmail = $(this).data("email");
+            var personAliasEmail = $(this).data("aliasEmail");
+            var personPhone = $(this).data("phone");
+            var personLocation = $(this).data("location");
+            var personFax = $(this).data("fax");
+            var personWebsite = $(this).data("website");
+            var personPub = $(this).data("publishable");
+
+            $("#edit-username").val(personUsername);
+            $("#edit-person-name").val(personName);
+            $("#edit-person-name-of-record").val(personNameOfRecord);
+            $("#edit-person-job-title").val(personJobTitle);
+            $("#edit-person-email").val(personEmail);
+            $("#edit-person-alias-email").val(personAliasEmail);
+            $("#edit-person-phone").val(personPhone);
+            $("#edit-person-location").val(personLocation);
+            $("#edit-person-fax").val(personFax);
+            $("#edit-person-website").val(personWebsite);
+            $("#edit-person-publishable option").filter(function() {
+                return $(this).text() === personPub;
+            }).prop('selected', true);
+
+            $("#editPersonModal").modal("show");
+
+            var editUrl = "{{ route('person_listings.update', ':personUsername') }}";
+            editUrl = editUrl.replace(":personUsername", personUsername);
+            $("#editPersonModal form").attr("action", editUrl);
+        });
 
     
 </script>
+
+@endsection
